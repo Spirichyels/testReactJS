a25e79b83c4438c42fd1ece40efb55b8c51ea877
Spirich EZOX <ya.govnosp4@yandex.ru>
2024-06-19 10:05:49 +0500

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://w7.pngwing.com/pngs/876/201/png-transparent-logo-graphic-designer-logos-company-logo-wikimedia-commons.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://vsegda-pomnim.com/uploads/posts/2022-04/1649336918_29-vsegda-pomnim-com-p-krasivie-plyazhi-mira-foto-35.jpg"
+          alt=""
+        />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My Post
+        <div>New Post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+          <div>Post 3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
